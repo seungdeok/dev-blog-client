b315e90aace22755b4ed28a073e0850a07d1afca
seungdeok <akk9132@naver.com>
2023-09-12 20:59:17 +0900

    style: Update markdown rederer style

--- src/app/admin/posts/page.tsx
+++ src/app/admin/posts/page.tsx
@@ -1,69 +1,32 @@
 'use client';
 
 import Link from 'next/link';
-import { styled } from 'styled-components';
 import { AdminPostCard } from '@/components/card/AdminPostCard';
 import { Post } from '@/types/Post';
 import { useEffect, useState } from 'react';
 import { postAPI } from '@/api/post';
 import WithAuth from '@/components/hocs/WithAuth';
-
-const S = {
-  container: styled.div`
-    ${({ theme }) =>
-      theme.MIXINS.flexBox('column', 'flex-start', 'flex-start')};
-    height: 100%;
-    width: 100%;
-  `,
-  heading: styled.h1`
-    font-weight: bold;
-    font-size: 32px;
-    width: 100%;
-    text-align: center;
-    color: ${({ theme }) => theme.colors.text['222222']};
-    margin-top: 12px;
-  `,
-  section: styled.section`
-    width: 100%;
-    margin-top: 48px;
-    ${({ theme }) =>
-      theme.MIXINS.flexBox('column', 'flex-start', 'flex-start')};
-  `,
-  sectionColContent: styled.ul`
-    width: 100%;
-    margin-top: 12px;
-    
-    a {
-      ${({ theme }) =>
-        theme.MIXINS.flexBox('column', 'flex-start', 'flex-start')};
-      margin-bottom: 16px;
-    }
-  `,
-  floatingAction: styled.div`
-    position: fixed;
-    right: 16px;
-    bottom: 66px;
-
-    a {
-      background-color: ${({ theme }) => theme.colors.primary};
-      width: 52px;
-      height: 52px;
-      ${({ theme }) => theme.MIXINS.flexBox('column', 'center', 'center')};
-      border-radius: 50%;
-
-      svg {
-        height: 24px;
-      }
-    }
-  `,
-};
+import * as S from './page.style';
+import { Pagination } from '@/components/Pagination';
+import { useSearchParams } from 'next/navigation';
+import { LoadingSkeleton } from '@/components/loading/LoadingSkeleton';
 
 export default function AdminPostPage() {
+  const [loading, setLoading] = useState(true);
   const [postList, setPostList] = useState<Post[]>([]);
+  const [total, setTotal] = useState(0);
+
+  const searchParams = useSearchParams();
+  const page = searchParams.get('page');
+  const currentPage = page ? Number(page) : 1;
+  const maxPage = Math.ceil(total / 10);
 
   async function initRequest() {
-    const postResponse = await postAPI.list();
+    const postResponse = await postAPI.list(currentPage);
+
+    setLoading(false);
     setPostList([...postResponse.data]);
+    setTotal(postResponse.total);
   }
 
   const handleDelete =
@@ -84,18 +47,32 @@ export default function AdminPostPage() {
       <S.container>
         <S.heading>포스트 관리</S.heading>
         <S.section>
-          <S.sectionColContent>
-            {postList.map(item => (
-              <Link key={item.id} href={`/admin/posts/${item.id}`}>
-                <AdminPostCard
-                  title={item.title}
-                  draft={item.draft}
-                  modified_at={item.modified_at}
-                  onDelete={handleDelete(item.id)}
-                />
-              </Link>
-            ))}
-          </S.sectionColContent>
+          {loading ? (
+            <S.sectionColContent>
+              <LoadingSkeleton
+                count={5}
+                styles={{
+                  padding: '16px',
+                  margin: '8px 0',
+                  width: '100%',
+                  height: 88,
+                }}
+              />
+            </S.sectionColContent>
+          ) : (
+            <S.sectionColContent>
+              {postList.map(item => (
+                <Link key={item.id} href={`/admin/posts/${item.id}`}>
+                  <AdminPostCard
+                    title={item.title}
+                    draft={item.draft}
+                    modified_at={item.modified_at}
+                    onDelete={handleDelete(item.id)}
+                  />
+                </Link>
+              ))}
+            </S.sectionColContent>
+          )}
         </S.section>
         <S.floatingAction>
           <Link href="/admin/write">
@@ -109,6 +86,9 @@ export default function AdminPostPage() {
             </svg>
           </Link>
         </S.floatingAction>
+        <S.pagination>
+          <Pagination maxPage={maxPage} currentPage={currentPage} />
+        </S.pagination>
       </S.container>
     </WithAuth>
   );
--- src/components/markdown/Renderer.tsx
+++ src/components/markdown/Renderer.tsx
@@ -1,9 +1,11 @@
-import MDEditor from '@uiw/react-md-editor';
+// import MDEditor from '@uiw/react-md-editor';
+import MarkdownPreview from '@uiw/react-markdown-preview';
+import '@uiw/react-markdown-preview/markdown.css';
 
 interface Props {
   data: string;
 }
 
 export function Renderer({ data }: Props) {
-  return <MDEditor.Markdown source={data} style={{ whiteSpace: 'pre-wrap' }} />;
+  return <MarkdownPreview source={data} style={{ whiteSpace: 'pre-wrap' }} />;
 }
